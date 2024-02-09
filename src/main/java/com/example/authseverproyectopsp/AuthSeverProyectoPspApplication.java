e609a92cf2266d7ca1f8ff09b1b8cf1440581ae4
lud0va <ovando.decena.luis@gmail.com>
2024-02-09 09:32:08 +0100

    s

---
+++ src/main/java/com/example/authseverproyectopsp/AuthSeverProyectoPspApplication.java
@@ -0,0 +1,13 @@
+package com.example.authseverproyectopsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSeverProyectoPspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSeverProyectoPspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authseverproyectopsp/AuthSeverProyectoPspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authseverproyectopsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSeverProyectoPspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
